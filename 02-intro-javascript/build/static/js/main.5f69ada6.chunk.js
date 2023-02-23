33088115e907f0c2a80e38a69ccce65257160d5e
Heimer Martinez <hmartinez@cafetosoftware.com>
2023-02-23 16:06:47 -0500

    import export y funciones comunes de arreglos

---
+++ 02-intro-javascript/build/static/js/main.5f69ada6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpintro-javascript"]=this["webpackJsonpintro-javascript"]||[]).push([[0],[function(n,e,a){"use strict";a.r(e);console.log([{id:1,name:"Batman",owner:"DC"},{id:2,name:"Spiderman",owner:"Marvel"},{id:3,name:"Superman",owner:"DC"},{id:4,name:"Flash",owner:"DC"},{id:5,name:"Wolverine",owner:"Marvel"}])}],[[0,1]]]);
+//# sourceMappingURL=main.5f69ada6.chunk.js.map
--- 02-intro-javascript/src/bases/04-arreglos.js
+++ 02-intro-javascript/src/bases/04-arreglos.js
@@ -1,14 +1,23 @@
-const arreglo = [1, 2, 3, 4]
+const arreglo = [ 1, 2, 3, 4 ]
 
-let arreglo2 = [...arreglo, 5];
+let arreglo2 = [ ...arreglo, 5 ];
+
+
+// arreglo.push(1)
+// arreglo.push( 2 )
+// arreglo.push( 3 )
+// arreglo.push( 4 )
+// En vez de insertar el elemnto con push se usa mejor el spread ... para manipular mejor
 
 
 
-const arreglo3 = arreglo2.map(function(numero) {
+
+const arreglo3 = arreglo2.map( function ( numero )
+{
     return numero * 2;
-});
+} );
 
 
-console.log(arreglo);
-console.log(arreglo2);
-console.log(arreglo3);
+console.log( arreglo );
+console.log( arreglo2 );
+console.log( arreglo3 );
